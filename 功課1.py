11c10c395212d11f73c9a9d0ea690411d282da7d
yichenwang0001 <87683387+yichenwang0001@users.noreply.github.com>
2021-07-20 11:20:01 +0800

    Add files via upload

---
+++ 功課1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 12:19:50 2021
+
+@author: eason
+"""
+
+s=input('半徑')
+s=float(s)
+
+a=s*2*3.1415926535897932384626433832795028841971693993751053
+
+print('圓周'+str(a))
